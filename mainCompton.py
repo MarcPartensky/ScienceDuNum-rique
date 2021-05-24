086e94dd7dac63b2992b35e4338ed71a82ed27dc
Spaectus <alex-ad.bigot@laposte.net>
2021-05-24 19:51:44 +0200

    main Compton

--- mainCompton.py
+++ mainCompton.py
@@ -1,25 +1,65 @@
 import random, pygame,math
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+
+
+
 class Electron(pygame.sprite.Sprite):
-    def __init__(self):
-        pass
+    def __init__(self, x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/electron.png").convert()
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect = self.image.get_rect()
+        self.x=x
+        self.y=SCREEN_HEIGHT/2
+        self.inMovement=True
+
+    def update(self, *args, **kwargs) -> None:
+        newpos = self.rect.move((self.x, self.y))
+        self.rect = newpos
+        pygame.event.pump()
+
 
 class Main():
+
     def __init__(self):
-        SCREEN_WIDTH = 800
-        SCREEN_HEIGHT = 600
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))#, flags=pygame.RESIZABLE)
         pygame.display.set_caption("Effet Compton")
-        background = pygame.Surface(self.screen.get_size())
-        background = background.convert()
-        background.fill((255,)*3)
-        self.screen.blit(background, (40,30))
+        self.make_background()
+        self.electron = Electron(0)
+        self.electron_sprite= pygame.sprite.RenderPlain(self.electron)
+        self.screen.blit(self.background, (0,0))
         pygame.display.flip()
+
+    @property
+    def w(self):
+        return self.screen.get_width()
+
+    @property
+    def h(self):
+        return self.screen.get_height()
+
+    def make_background(self):
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((255,) * 3)
+        pygame.draw.line(self.background, (0,)*3, (0, self.h/2), (self.w, self.h/2))
+
 
     def __call__(self, *args, **kwargs):
         game_exit = False
+        clock = pygame.time.Clock()
         while not game_exit:
+            clock.tick(60)
+            self.screen.blit(self.background, self.electron)
+            self.electron.update()
+            self.electron_sprite.draw(self.screen)
+            pygame.display.flip()
             events = pygame.event.get()
 
             for event in events:
--- main_photoelectric.py
+++ main_photoelectric.py
@@ -1,28 +1,66 @@
 #!/usr/bin/env python
 
-import random, pygame, math
+import pygame
+import random
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
-class Main():
+class Photon:
+    def __init__(self, x, y, color=0x000000, radius=2):
+        """Create a photon."""
+        self.x = x
+        self.y = y
+        self.color = color
+        self.radius = radius
+
+    def show(self, screen):
+        """Show the photon."""
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+
+class Main:
     def __init__(self):
+        """Initializing the simulation."""
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode(flags=pygame.RESIZABLE)
         pygame.display.set_caption("Effet Compton")
-        background = pygame.Surface(self.screen.get_size())
-        background = background.convert()
-        background.fill((0, 0, 0))
+        self.photon = Photon(self.w / 2, self.h / 2)
+        self.background_color = 0x000000
+
+    @property
+    def w(self):
+        """Return the width."""
+        return self.screen.get_width()
+
+    @property
+    def h(self):
+        """Return the height."""
+        return self.screen.get_height()
 
     def __call__(self, *args, **kwargs):
+        """Main loop."""
         while True:
-#             events = pygame.event.get()
+            events = pygame.event.get()
 
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                    raise SystemExit
+                # elif event.type == pygame.VIDEORESIZE:
+
+            self.update()
+            self.show()
+
+    def update(self):
+        """Update the simulation."""
+        pass
+
+    def show(self):
+        """Show the simulation."""
+        self.screen.fill(self.background_color)
+        self.photon.show(self.screen)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
-    print('salut')
     m = Main()
     m()
