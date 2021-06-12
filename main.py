78a5a2e2e6493f48a795ec417422698a95f8226f
Spaectus <30037190+Spaectus@users.noreply.github.com>
2021-06-12 19:41:20 +0200

    compton inverse

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 import math
 from scipy import constants
 
-
 def isfloat(value):
     try:
         float(value)
@@ -47,8 +46,7 @@ class Main:
     def __init__(self):
         pass
 
-    def __call__(self, *args, **kwargs):
-        # LC Compton wavelength
+    def compton(self, *args, **kwargs):
         self.LC = constants.physical_constants["Compton wavelength"][0]
 
         print("Choisissez l'angle du photon diffusé en degré")
@@ -79,36 +77,42 @@ class Main:
         )
         print(txt)
 
-    def ics(self):
-        self.Ee = constants.electron_mass * (constants.c ** 2)
-        # self.lorentz=
-        self.E0_E = self.lorentz * self.E0(
-            1 - (self.v / constants.c) * math.cos(self.theta0)
-        )
-        self.E1_L = self.E0_E / (
-            1 + (self.E0_E / self.Ee) * (1 - math.cos(self.theta_E))
-        )
+    def comptonInverse(self):
 
-        self.E1 = self.lorentz * self.E1_L(
-            1 + (self.v / constants.c) * math.cos(self.theta1_E)
-        )
+        print("Energie de l'électron")
+        self.Ee = getFloat(0, 10**20, "J")
+
+        print("Energie du photon")
+        self.Ep = getFloat(0, 10**20, "J")
+
+        print("Angle du photon incident")
+        self.alpha = (getFloat(0, 90, "degré")) * (math.pi/180)
+        self.theta1 = math.pi - self.alpha
+
+        print("Angle pris par le photon rétro-diffusé")
+        self.theta2 = getFloat(0, 90, "degré") * (math.pi/180)
+
+        beta = 1
+
+        A = self.Ep * (1-beta*math.cos(self.theta1))
+        B = (1 - beta * math.cos(self.theta2))
+        C = (1-math.cos(self.theta2-self.theta1)) * (self.Ep/self.Ee)
+
+        self.Ex = A/(B+C)
+
+        print("Energie du photon diffusé : {} J".format(self.Ex))
+
 
     def photoelectrique(self):
 
-        # http://mdevmd.accesmad.org/mediatek/pluginfile.php/5037/mod_resource/content/2/Corrections%20des%20exercices%20sur%20leffet%20photo%C3%A9lectriqueTA%20-%20accesmad.htm
-
         print("Choisissez une longueur d'onde :")
-        self.l = getFloat(300, 1000, "en nm :") * (10 ** (-9))
-
-        # print("Choisissez une intensitée (%) :")
-        # self.i = getFloat(0, 100, "%")
+        self.l = getFloat(100, 1000, "en nm :") * (10 ** (-9))
 
         print(
             "Choisissez un matériau :"
         )  # to do utiliser curses https://docs.python.org/3/howto/curses.html
-        element = {"Cs": 1.19, "K": 2.29, "Na": 2.18, "Li": 2.39, "Zn": 4.3}
-        # txt = "\n".join(["{} : {}".format(elem[0], list(t.items()).index(elem)) for elem in element.items()])
-        # print(txt)
+        element = {"Cs": 1.19, "K": 2.29, "Na": 2.18, "Li": 2.39, "Zn": 4.3, "Co":3.90, "Al":4.08, "Pb":4.14, "Fe":4.50,
+                   "Cu":4.7, "Ag":4.73}
         print(",".join([e for e, _ in element.items()]))
         while True:
             result = input()
@@ -139,11 +143,8 @@ class Main:
 
 a = Main()
 while True:
-    print("Tapez 0 pour l'effet Compton et 1 pour l'effet photoelectrique")
-    choix = getInt(0, 1)
-    if choix == 0:
-        a()
-    else:
-        a.photoelectrique()
+    print("Tapez  :\n0 pour l'effet Compton\n1 pour l'effet photoelectrique\n2 pour l'effet compton inverse")
+    choix = getInt(0, 2)
+    [a.compton, a.photoelectrique, a.comptonInverse][choix]()
 
 input("Tapez sur enter pour quitter")
